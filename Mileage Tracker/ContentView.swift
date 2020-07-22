cf7217d63881965a0edf25b312f2c766524e2b2f
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-07-22 11:07:41 +0100

    Initial Commit

---
+++ Mileage Tracker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Mileage Tracker
+//
+//  Created by Alex Luna on 22/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mileage Tracker/Mileage_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mileage_TrackerApp.swift
+//  Mileage Tracker
+//
+//  Created by Alex Luna on 22/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct Mileage_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
