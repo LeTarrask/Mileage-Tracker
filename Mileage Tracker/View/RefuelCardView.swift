aa36823c94035da009f4a48136451457d2944603
leTarrask <contato@tarrask.com>
2021-03-23 16:59:53 +0000

    Redesigned card view

--- Mileage Tracker/View/RefuelCardView.swift
+++ Mileage Tracker/View/RefuelCardView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct RefuelCardView: View {
     var refuel: Refuel
-
+    
     var average: Double
-
+    
     var formatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
@@ -20,71 +20,80 @@ struct RefuelCardView: View {
     }()
 
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Text(NSLocalizedString("Refuel at: ", comment: ""))
-                    .font(.headline)
-                Text(formatter.string(from: refuel.creationDate))
+        ZStack {
+            RoundedRectangle(cornerRadius: 15.0)
+                .fill(LinearGradient(gradient: Gradient(colors: [Color("Redder"), Color("Wine")]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            VStack(alignment: .leading) {
+                HStack(alignment: .center) {
+                    Square(icon: "car", number: String(refuel.kmAdded.clean),
+                           value: "km",
+                           label: NSLocalizedString("Since last", comment: ""))
+                    Spacer()
+                    Square(icon: "drop.fill",
+                           number: String(refuel.liters),
+                           value: NSLocalizedString("L", comment: ""),
+                           label: NSLocalizedString("Fuel", comment: ""))
+                    Spacer()
+                    Square(icon: "eurosign.square",
+                           number: String(refuel.money),
+                           value: NSLocalizedString("€", comment: ""),
+                           label: NSLocalizedString("Payment", comment: ""))
+                    Spacer()
+                    // TO DO: this math here is very strange
+                    Square(icon: "drop",
+                           number: String((refuel.pricePerLiter-average).rounded(toPlaces: 2)),
+                           value: NSLocalizedString("€", comment: ""),
+                           label: NSLocalizedString("than average:", comment: ""))
+                }
+                .padding(.bottom)
+                HStack {
+                    Spacer()
+                    Square(icon: nil,
+                           number: String(refuel.pricePerLiter),
+                           value: "€",
+                           label: NSLocalizedString("per liter:", comment: ""))
+                    Square(icon: nil,
+                           number: String(refuel.totalKM),
+                           value: NSLocalizedString("km", comment: ""),
+                           label: NSLocalizedString("Total", comment: ""))
+                }
+                .padding(.bottom)
+                HStack {
+                    Text(NSLocalizedString("Refuel at: ", comment: ""))
+                        .font(.caption)
+                    Text(formatter.string(from: refuel.creationDate))
+                        .font(.caption)
+                }
+                .foregroundColor(Color("Cream"))
+            }.padding()
+        }
+    }
+}
+
+struct Square: View {
+    let icon: String?
+    let number: String
+    let value: String
+    let label: String
+
+    var body: some View {
+        VStack(alignment: .center) {
+            if icon != nil {
+                Image(systemName: icon!)
+                    .font(.largeTitle)
+            }
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(number)
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                    Text(value)
+                }
+                Text(label)
                     .font(.caption)
-            }.padding(.bottom)
-            HStack(alignment: .center) {
-                VStack {
-                    Text(NSLocalizedString("Total", comment: ""))
-                        .font(.caption)
-                        .fontWeight(.bold)
-                    HStack {
-                        Text(String(refuel.totalKM))
-                        Text(NSLocalizedString("km", comment: ""))
-                    }.font(.subheadline)
-                }
-                Spacer()
-                VStack {
-                    Text(NSLocalizedString("Since last", comment: ""))
-                        .font(.caption)
-                        .fontWeight(.bold)
-                    HStack {
-                        Text(String(refuel.kmAdded.clean))
-                        Text(NSLocalizedString("km", comment: ""))
-                    }.font(.subheadline)
-                }
-                Spacer()
-                VStack {
-                    Text(NSLocalizedString("Fuel", comment: ""))
-                        .font(.caption)
-                        .fontWeight(.bold)
-                    HStack {
-                        Text(String(refuel.liters))
-                        Text(NSLocalizedString("L", comment: ""))
-                    }.font(.subheadline)
-                }
-                Spacer()
-                VStack {
-                    Text(NSLocalizedString("Payment", comment: ""))
-                        .font(.caption)
-                        .fontWeight(.bold)
-                    HStack {
-                        Text(String(refuel.money))
-                        Text(NSLocalizedString("€", comment: ""))
-                    }.font(.subheadline)
-                }
-            }.padding(.bottom)
-            HStack {
-                Text(NSLocalizedString("Price per liter:", comment: ""))
-                    .font(.headline)
-                HStack {
-                    Text(String(refuel.pricePerLiter))
-                    Text(NSLocalizedString("€", comment: ""))
-                }
             }
-            HStack {
-                Text(NSLocalizedString("Difference from average:", comment: ""))
-                    .font(.headline)
-                HStack {
-                    Text(String((refuel.pricePerLiter-average).rounded(toPlaces: 2)))
-                    Text(NSLocalizedString("€", comment: ""))
-                }
-            }
-        }.padding(.bottom)
+        }
+        .foregroundColor(Color("Cream"))
     }
 }
 
@@ -92,5 +101,6 @@ struct RefuelCardView_Previews: PreviewProvider {
     static var previews: some View {
         let refuel = Refuel(totalKM: 2329, liters: 23.99, money: 23.09, kmAdded: 123)
         return RefuelCardView(refuel: refuel, average: 1.45)
+            .previewLayout(.sizeThatFits)
     }
 }
