1ad1bf9b61734183fac76352b5f3adb433d1edc5
leTarrask <contato@tarrask.com>
2021-03-26 14:28:10 +0000

    Implemented ViewRouter for Onboarding

--- Mileage Tracker/MileageTrackerApp.swift
+++ Mileage Tracker/MileageTrackerApp.swift
@@ -13,6 +13,8 @@ struct MileageTrackerApp: App {
 
     @StateObject var themeMG: ThemeManager = ThemeManager.shared
 
+    private var viewRouter: ViewRouter = ViewRouter()
+
     /// VIEW STRINGS & URLs
     let refuelsString = NSLocalizedString("Refuels", comment: "")
     let otherCostsString = NSLocalizedString("Other costs", comment: "")
@@ -21,29 +23,36 @@ struct MileageTrackerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            TabView {
-                MileageView(tracker: tracker) {
-                    tracker.save()
-                }.tabItem {
-                    Image(systemName: "drop.fill")
-                    Text(refuelsString)
-                }.foregroundColor(themeMG.theme.mainColor)
-                OtherCostsView(tracker: tracker) {
-                    tracker.save()
-                }.tabItem {
-                    Image(systemName: "wrench.and.screwdriver")
-                    Text(otherCostsString)
-                }.foregroundColor(themeMG.theme.mainColor)
-                GraphicsView(tracker: tracker)
-                    .tabItem {
-                        Image(systemName: "list.star")
-                        Text(statsString)
-                    }.foregroundColor(themeMG.theme.mainColor)
-                SettingsView(tracker: tracker)
-                    .tabItem {
-                        Image(systemName: "gearshape")
-                        Text(settingsString)
-                    }.foregroundColor(themeMG.theme.mainColor)
+            VStack {
+                if viewRouter.currentPage == "onboardingView" {
+                    OnboardingView(pages: OnboardingPage.fullOnboarding)
+                } else if viewRouter.currentPage == "homeView" {
+                    TabView {
+                        MileageView(tracker: tracker) {
+                            tracker.save()
+                        }.tabItem {
+                            Image(systemName: "drop.fill")
+                            Text(refuelsString)
+                        }.foregroundColor(themeMG.theme.mainColor)
+                        OtherCostsView(tracker: tracker) {
+                            tracker.save()
+                        }.tabItem {
+                            Image(systemName: "wrench.and.screwdriver")
+                            Text(otherCostsString)
+                        }.foregroundColor(themeMG.theme.mainColor)
+                        GraphicsView(tracker: tracker)
+                            .tabItem {
+                                Image(systemName: "list.star")
+                                Text(statsString)
+                            }.foregroundColor(themeMG.theme.mainColor)
+                        SettingsView(tracker: tracker)
+                            .tabItem {
+                                Image(systemName: "gearshape")
+                                Text(settingsString)
+                            }.foregroundColor(themeMG.theme.mainColor)
+                    }
+    
+                }
             }
             .accentColor(themeMG.theme.mainColor)
             .edgesIgnoringSafeArea(.top)
@@ -52,4 +61,17 @@ struct MileageTrackerApp: App {
             }
         }
     }
+}
+
+class ViewRouter: ObservableObject {
+    init() {
+        if !UserDefaults.standard.bool(forKey: "didLaunchBefore") {
+            UserDefaults.standard.set(true, forKey: "didLaunchBefore")
+            currentPage = "onboardingView"
+        } else {
+            currentPage = "homeView"
+        }
+    }
+
+    @Published var currentPage: String
 }
