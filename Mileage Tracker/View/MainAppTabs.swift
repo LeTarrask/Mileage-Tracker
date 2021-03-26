b5a62ad1130eb2fd1cbecb6eb0448f30710d19bc
leTarrask <contato@tarrask.com>
2021-03-26 14:39:57 +0000

    Added link to in-app purchase implementation

---
+++ Mileage Tracker/View/MainAppTabs.swift
@@ -0,0 +1,20 @@
+//
+//  MainAppTabs.swift
+//  Mileage Tracker
+//
+//  Created by Alex Luna on 26/03/2021.
+//
+
+import SwiftUI
+
+struct MainAppTabs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainAppTabs_Previews: PreviewProvider {
+    static var previews: some View {
+        MainAppTabs()
+    }
+}
