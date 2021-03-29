efa2f17a1c2fcbd55a251f7c3bfd61af5190d662
leTarrask <contato@tarrask.com>
2021-03-29 10:39:30 +0100

    added flexible view to graphics view

--- Mileage Tracker/View/Main Tab Views/GraphicsView.swift
+++ Mileage Tracker/View/Main Tab Views/GraphicsView.swift
@@ -27,70 +27,78 @@ struct GraphicsView: View {
                 InterstitialView()
             } else {
                 // MARK: - Regular Screen
-                ScrollView {
-                    VStack {
-                        // MARK: - Averages display
-                        ZStack {
-                            BackgroundCard()
-                                .frame(maxHeight: 140)
-                            VStack(alignment: .center) {
-                                Text("Total KM: " + String(tracker.totalKM.clean) + " km")
-                                    .foregroundColor(themeMG.theme.highlightColor)
-                                    .font(.title)
-                                    .fontWeight(.bold)
-                                HStack {
-                                    VStack(alignment: .leading) {
-                                        Square(icon: nil,
-                                               number: tracker.averageConsumption,
-                                               value: "km/L",
-                                               label: "Average consumption")
-                                        Square(icon: nil,
-                                               number: tracker.averageSpending,
-                                               value: "km/€",
-                                               label: "Average spending")
+                VStack {
+                    GeometryReader { reader in
+                        VStack {
+                            // MARK: - Averages display
+                            ZStack {
+                                BackgroundCard()
+                                    .frame(maxHeight: 140)
+                                VStack(alignment: .center) {
+                                    Text("Total KM: " + String(tracker.totalKM.clean) + " km")
+                                        .foregroundColor(themeMG.theme.highlightColor)
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                    HStack {
+                                        VStack(alignment: .leading) {
+                                            Square(icon: nil,
+                                                   number: tracker.averageConsumption,
+                                                   value: "km/L",
+                                                   label: "Average consumption")
+                                            Square(icon: nil,
+                                                   number: tracker.averageSpending,
+                                                   value: "km/€",
+                                                   label: "Average spending")
+                                        }
+                                        Spacer()
+                                        VStack {
+                                            Square(icon: nil,
+                                                   number: tracker.totalSpending,
+                                                   value: "€",
+                                                   label: "Total fuel spending")
+                                            Square(icon: nil,
+                                                   number: tracker.averagePrice,
+                                                   value: "€/l",
+                                                   label: "Average Fuel Price")
+                                        }
                                     }
-                                    Spacer()
-                                    VStack {
-                                        Square(icon: nil,
-                                               number: tracker.totalSpending,
-                                               value: "€",
-                                               label: "Total fuel spending")
-                                        Square(icon: nil,
-                                               number: tracker.averagePrice,
-                                               value: "€/l",
-                                               label: "Average Fuel Price")
-                                    }
-                                }
-                            }.padding()
-                        }.padding()
-                        // MARK: - Graphics Selector
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 7)
-                                .fill(themeMG.theme.backgroundColor)
-                                .frame(maxHeight: 30)
-                                .shadow(color: .black, radius: 1, x: 1, y: 1)
-                            HStack {
-                                Button(action: {graphType = .spending}, label: {
-                                    Text("Refuel Cost")
-                                })
-                                Spacer()
-                                Button(action: {graphType = .dates}, label: {
-                                    Text("Price per liter")
-                                })
-                                Spacer()
-                                Button(action: {graphType = .km}, label: {
-                                    Text("Kms per refuel")
-                                })
+                                }.padding()
                             }
-                            .foregroundColor(themeMG.theme.secondaryColor)
                             .padding()
-                        }.padding()
-                        // MARK: - Graphic
-                        Graphic(tracker: tracker, type: $graphType)
-                            .padding()
+                            .frame(width: .infinity, height: reader.size.width * 0.40)
+                            // MARK: - Graphics Selector
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 7)
+                                    .fill(themeMG.theme.backgroundColor)
+                                    .frame(maxHeight: 30)
+                                    .shadow(color: .black, radius: 1, x: 1, y: 1)
+                                HStack {
+                                    Button(action: {graphType = .spending}, label: {
+                                        Text("Refuel Cost")
+                                    })
+                                    Spacer()
+                                    Button(action: {graphType = .dates}, label: {
+                                        Text("Price per liter")
+                                    })
+                                    Spacer()
+                                    Button(action: {graphType = .km}, label: {
+                                        Text("Kms per refuel")
+                                    })
+                                }
+                                .foregroundColor(themeMG.theme.secondaryColor)
+                                .padding()
+                            }.padding()
+                            .frame(width: .infinity, height: reader.size.width * 0.1)
+                            // MARK: - Graphic
+                            Graphic(tracker: tracker, type: $graphType)
+                                .padding()
+                                .frame(width: .infinity, height: reader.size.width * 0.55)
+                        }
                     }
                     // MARK: - Ad Banner
-                    Banner()
+                    if !tracker.paidApp {
+                        Banner()
+                    }
                 }
             }
         }
@@ -110,6 +118,7 @@ struct GraphicsView_Previews: PreviewProvider {
     static var previews: some View {
         let view = GraphicsView(tracker: MileageTracker())
         view.tracker.refuels = Refuel.data
+        view.tracker.paidApp = false
         return view
     }
 }
