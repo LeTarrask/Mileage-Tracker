2d238a7fb5be7cca6427cb2a02f8b449c3861c2d
leTarrask <contato@tarrask.com>
2021-03-29 15:42:49 +0100

    Added elements to view

--- Mileage Tracker/View/Onboarding/WelcomeOnboarding.swift
+++ Mileage Tracker/View/Onboarding/WelcomeOnboarding.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct WelcomeOnboarding: View {
     var body: some View {
-        Text("Welcome")
+        VStack(alignment: .center) {
+            Image(systemName: "drop.fill")
+                .resizable()
+                .frame(width: 200, height: 300)
+            Text("Main headline")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            Text("Main headline")
+                .font(.body)
+                .foregroundColor(.gray)
+        }
     }
 }
 
