b49e1f3c6f89fd29e70cbccd7f345870d4c8ceab
leTarrask <tarrask@tarrask.com>
2023-02-08 16:27:10 +0000

    unused struct ??? should we delete?

--- Mileage Tracker/View/Helper Views/Square.swift
+++ Mileage Tracker/View/Helper Views/Square.swift
@@ -7,36 +7,36 @@
 
 import SwiftUI
 
-struct Square: View {
-    @StateObject var themeMG: ThemeManager = ThemeManager.shared
-    
-    let icon: String?
-    let number: Double
-    let value: String
-    let label: String
-    var flip: Bool {
-        icon == "triangle.fill"
-    }
-
-    var body: some View {
-        VStack(alignment: .center) {
-            if icon != nil {
-                Image(systemName: icon!)
-                    .font(.largeTitle)
-                    .rotationEffect(number < 0 ? .degrees(-180) : .zero)
-                    .foregroundColor(number < 0 ? .green : flip ? .red : themeMG.theme.backgroundColor)
-            }
-            VStack(alignment: .leading) {
-                HStack {
-                    Text(String(number.rounded(toPlaces: 2)))
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                    Text(value)
-                }
-                Text(label)
-                    .font(.caption)
-            }
-        }
-        .foregroundColor(themeMG.theme.mainColor)
-    }
-}
+//struct Square: View {
+//    @StateObject var themeMG: ThemeManager = ThemeManager.shared
+//    
+//    let icon: String?
+//    let number: Double
+//    let value: String
+//    let label: String
+//    var flip: Bool {
+//        icon == "triangle.fill"
+//    }
+//
+//    var body: some View {
+//        VStack(alignment: .center) {
+//            if icon != nil {
+//                Image(systemName: icon!)
+//                    .font(.largeTitle)
+//                    .rotationEffect(number < 0 ? .degrees(-180) : .zero)
+//                    .foregroundColor(number < 0 ? .green : flip ? .red : themeMG.theme.backgroundColor)
+//            }
+//            VStack(alignment: .leading) {
+//                HStack {
+//                    Text(String(number.rounded(toPlaces: 2)))
+//                        .font(.subheadline)
+//                        .fontWeight(.bold)
+//                    Text(value)
+//                }
+//                Text(label)
+//                    .font(.caption)
+//            }
+//        }
+//        .foregroundColor(themeMG.theme.mainColor)
+//    }
+//}
