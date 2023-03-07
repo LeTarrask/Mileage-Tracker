0be6b0e8b99de1580e105763f9ad0d7f323b4c1f
Alex Luna <contato@tarrask.com>
2023-03-07 17:37:09 +0000

    updated and run tests

--- Mileage TrackerTests/MileageTrackerTests.swift
+++ Mileage TrackerTests/MileageTrackerTests.swift
@@ -9,25 +9,34 @@ import XCTest
 @testable import Mileage_Tracker
 
 class MileageTrackerTests: XCTestCase {
+    let tracker = MileageTracker()
 
     func testTracker() throws {
-        let tracker = MileageTracker()
         tracker.refuels = Refuel.data
-        XCTAssertEqual(tracker.refuels.count, 20, "Tracker doesn't have test Refuels")
+        XCTAssertEqual(tracker.refuels.count, 35, "Tracker didn't load test Refuels")
         tracker.otherCosts = OtherCost.data
-        XCTAssertEqual(tracker.otherCosts.count, 5, "Tracker doesn't have test otherCosts")
-
+        XCTAssertEqual(tracker.otherCosts.count, 5, "Tracker didn't load test otherCosts")
+    }
+    
+    func testGenerateCSV() throws {
         let csv = tracker.generateCSV()
         XCTAssertNotNil(csv, "Tracker generates string to store CSV")
+    }
+    
+    func testTrackerLoadTestData() throws {
+        tracker.loadTestData()
 
+        XCTAssertEqual(tracker.refuels.count, 35, "Tracker didn't load test data")
+    }
+    
+    func testDeleteData() throws {
+        tracker.loadTestData()
         tracker.deleteData()
 
         XCTAssertEqual(tracker.refuels.count, 0, "Tracker didn't delete data")
-
-        tracker.loadTestData()
-
-        XCTAssertEqual(tracker.refuels.count, 20, "Tracker didn't load test data")
-
+    }
+    
+    func testTrackerSave() throws {
         tracker.save()
 
         let file = tracker.csvFile()
